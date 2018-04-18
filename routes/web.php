ec4e7402b7963dc9d142200f63a7a4bf9b39638b
viniciusbda_ <vinicius.bda@icloud.com>
2018-04-18 08:25:47 -0300

    test route

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,12 @@
 
 $router->post('/', 'mapsController@index');
 $router->get('/auth', 'AuthController@getToken');
+$router->get('/test',function()
+{
+	return response()->json([
+		"statusCode" => 200,
+		"message" => "successfully tested"
+	],200);
+});
 
 
